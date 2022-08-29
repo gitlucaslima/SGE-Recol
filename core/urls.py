aed14d6a81a07d5753dcf1f6d978dc74a0016839
Lucas Lima <15lucas.lima15@gmail.com>
2022-08-29 10:54:55 -0500

    criação pagina colaborador e equipamento

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from core.views import index
+from core.views import colaborador, equipamento, index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('colaborador/', colaborador, name='colaborador'),
+    path('equipamento/', equipamento, name='equipamento')
+
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, template_name='base.html')
+
+def colaborador(request):
+    return render(request, template_name='colaborador/colaborador.html')
+
+def equipamento(request):
+    return render(request, template_name='equipamento/equipamento.html')
