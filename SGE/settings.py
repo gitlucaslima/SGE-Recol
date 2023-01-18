831ecda7d6e369e16453041049f141947eab2fdd
Lucas Lima <15lucas.lima15@gmail.com>
2023-01-18 15:05:57 -0500

    debug set false

--- SGE/settings.py
+++ SGE/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0cg*%t#d=s7g4^tl&1$_$5rh0g$0up0@_604(6g+-n()$=t$)%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
