29051cb5b60e0b94cd12963b1cd32400aa3dd150
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2023-01-18 19:31:56 -0500

    asdas

--- SGE/settings.py
+++ SGE/settings.py
@@ -109,9 +109,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR, 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR, 'static'
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
