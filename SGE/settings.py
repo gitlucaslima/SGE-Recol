7dad1a8a0fdef16d4e537019f74798260280a902
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2023-01-18 22:43:35 -0500

    deploy

--- SGE/settings.py
+++ SGE/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
