3f3e1fcb77151a5a1361811d68b55085599f0924
Lucas Lima <15lucas.lima15@gmail.com>
2023-01-19 08:33:58 -0500

    deploy

--- SGE/settings.py
+++ SGE/settings.py
@@ -64,6 +64,7 @@ WSGI_APPLICATION = 'SGE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Produção
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -74,6 +75,19 @@ DATABASES = {
         'PORT': '7849',
     }
 }
+
+# Local
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
