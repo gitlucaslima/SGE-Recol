f244c596fe24c54e16a433c315cf9fb522620f4b
Lucas Lima <15lucas.lima15@gmail.com>
2023-01-19 09:45:44 -0500

    deploy

--- SGE/settings.py
+++ SGE/settings.py
@@ -8,7 +8,8 @@ mimetypes.add_type("text/css", ".css", True)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
