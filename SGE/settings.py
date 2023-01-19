049779bc47c8b2e7aa45d73849790f442d39e5b1
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2023-01-18 20:39:36 -0500

    static deploy whitesr

--- SGE/settings.py
+++ SGE/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR, 'static'
+    os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
